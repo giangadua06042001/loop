9c9e80dcdcbaee77204437895aa50fc679c8dafd
giangadua06042001@gmai.com <abc@gmail.com>
2023-03-23 11:12:47 +0700

    scr/Main1.java

--- src/Main1.java
+++ src/Main1.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Main1 {
+import java.util.Scanner;
+
+public class Main1 {
+    public static void main(String[] args) {
+       for ( int i =2; i<100;i++){
+           if (checkPrime(i)==true){
+               System.out.println(i+"la snt");
+           }
+       }
+
+    }
+
+    public static  boolean checkPrime( int number) {
+        boolean check=true;
+        for(int i =2; i<number;i++){
+            if(number % i == 0){
+                check =false;
+            }
+        }
+        return check;
+    }
 }
+
